b0038d1253e824b1492826ac7b8248e65b8f718f
Adsigma <hong@cloudjetkpi.com>
2021-10-11 14:29:19 +0700

    scripts

--- scripts/encoding_functions.js
+++ scripts/encoding_functions.js
@@ -1,8 +1,8 @@
-import Web3 from 'web3';
+const Web3 = require('web3');
 
 const web3 = new Web3();
 
-const methodId = web3.utils.keccak256('testCall(string)').substr(0, 10); //0xc7cee1b7
+const methodId = web3.utils.keccak256('updateDelay(uint256)').substr(0, 10); //0xc7cee1b7
 
 // In this function call, the first and only parameter uses a dynamic type: string
 // https://docs.soliditylang.org/en/latest/abi-spec.html#use-of-dynamic-types
@@ -10,10 +10,29 @@ const methodId = web3.utils.keccak256('testCall(string)').substr(0, 10); //0xc7c
 // we use the offset in bytes to the start of their data area, measured from the start of the value encoding
 // offset to start of data part of second parameter, 1*32 bytes, exactly the size of the head part
 
-const stringArgument = web3.utils.padLeft(web3.utils.toHex(1*32), 64).replace('0x', '');
+const uintArgument = web3.eth.abi.encodeParameter('uint256', 10).replace('0x', '');;
 // 0000000000000000000000000000000000000000000000000000000000000020
 
 
-const stringValue = 'TODO' // This one is a bit tricky, the docs explain it well: https://docs.soliditylang.org/en/latest/abi-spec.html#use-of-dynamic-types
+const call = methodId + uintArgument;
+console.log(call);
 
-const call = methodId + stringArgument + stringValue;
+
+// upgrade contract 
+
+
+const methodId2 = web3.utils.keccak256('upgrade(address,address)').substr(0, 10); //0xc7cee1b7
+
+// In this function call, the first and only parameter uses a dynamic type: string
+// https://docs.soliditylang.org/en/latest/abi-spec.html#use-of-dynamic-types
+
+// we use the offset in bytes to the start of their data area, measured from the start of the value encoding
+// offset to start of data part of second parameter, 1*32 bytes, exactly the size of the head part
+
+const addressArgument21 = web3.eth.abi.encodeParameter('address', '0x547CBE0f0c25085e7015Aa6939b28402EB0CcDAC').replace('0x', '');;
+const addressArgument22 = web3.eth.abi.encodeParameter('address', '0x53319829B15cfF46868d5c3970CD56FA7594E0D7').replace('0x', '');;
+// 0000000000000000000000000000000000000000000000000000000000000020
+
+
+const call2 = methodId2 + addressArgument21+addressArgument22;
+console.log(call2);
